a9bd85f5ca01d086f63acecc54c0cd6e789060c8
Joel-Lo <115680393+Joel-Lo@users.noreply.github.com>
2025-11-14 14:38:30 +0800

    Update Hangman.js

--- Hangman.js
+++ Hangman.js
@@ -72,12 +72,12 @@ const initGame = (button, clickedLetter) => {
     if(correctLetters.length === lowerCaseWord.length) return gameOver(true);
 }
 
-for (let i = 97; i <= 122; i++) {
-    const button = document.createElement("button");
-    button.innerText = String.fromCharCode(i);
-    keyboardDiv.appendChild(button);
-    button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
-}
+// for (let i = 97; i <= 122; i++) {
+//     const button = document.createElement("button");
+//     button.innerText = String.fromCharCode(i);
+//     keyboardDiv.appendChild(button);
+//     button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+// }
 
 
 Swal.fire({
